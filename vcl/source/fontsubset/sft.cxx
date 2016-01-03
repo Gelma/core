32abcf0fd4aecfdd6da96e68effa0782349ed32b
Andrea Gelmini <andrea.gelmini@gelma.net>
2016-01-03 15:08:35 +0100

    Fix typo
    Change-Id: Ib98d323994ed3cca8f2f5326b822b8e2a89af7a0

--- vcl/source/fontsubset/sft.cxx
+++ vcl/source/fontsubset/sft.cxx
@@ -1024,7 +1024,7 @@ static void GetNames(TrueTypeFont *t)
     }
 
     /* #i60349# sanity check psname
-     * psname parctically has to be 7bit ascii and should not contains spaces
+     * psname pratically has to be 7bit ascii and should not contains spaces
      * there is a class of broken fonts which do not fulfill that at all, so let's try
      * if the family name is 7bit ascii and take it instead if so
      */
